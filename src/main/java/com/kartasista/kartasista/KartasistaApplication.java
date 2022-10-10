ebdb7e2c24dbed21697e4ed7114f6bbfe0aa21c0
junior <jr0797024@gmail.com>
2022-10-10 19:47:52 -0300

    primeira versão do codigo

---
+++ src/main/java/com/kartasista/kartasista/KartasistaApplication.java
@@ -0,0 +1,13 @@
+package com.kartasista.kartasista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartasistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KartasistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kartasista/kartasista/KartasistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kartasista.kartasista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KartasistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
